c8e9fb8ca4cbcb45678ae5072a9445524742037d
riecenaidoo <riece@psybergate.co.za>
2025-03-05 23:04:29 +0200

    added GET/ servers
    At this stage I am not sure what the best practices for Flask are. Neither am I sure on how to structure this project.
    There may be many revisions to the structure, but I will have time to figure that all out.
    Progress on this project is bottlenecked by the frontend, so I will only be expanding this service as endpoints are needed by the frontend.

--- src/api.py
+++ src/api.py
@@ -8,6 +8,7 @@ class AudioServiceAPI(Flask):
     def __init__(self, client: AudioClient, import_name: str):
         super().__init__(import_name)
         self.client = client
+        self._add_routes()
 
     @to_thread
     def start(self, host, port):
@@ -17,3 +18,8 @@ class AudioServiceAPI(Flask):
         """
         self.run(host=host, port=port)
 
+    def _add_routes(self):
+        self.add_url_rule("/servers", view_func=self._get_servers)
+
+    def _get_servers(self) -> Response:
+        return jsonify([server.serialize() for server in self.client.get_servers()])
--- src/bot.py
+++ src/bot.py
@@ -11,9 +11,20 @@ _log.addHandler(utils.HANDLER)
 _log.setLevel(logging.INFO)
 
 
+class Server:
+    def __init__(self, server: Guild):
+        self.id = server.id
+        self.name = server.name
+
+    def serialize(self) -> dict:
+        return {"id": self.id, "name": self.name}
+
+
 class AudioClient(discord.Client):
     """Discord Client that manages the streaming of audio into voice channels."""
 
     def __init__(self, *, intents: Intents, **options: typing.Any):
         super().__init__(intents=intents, **options)
 
+    def get_servers(self) -> list[Server]:
+        return [Server(guild) for guild in self.guilds]
